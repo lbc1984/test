295f4fe4e1646471baf4a5d6d54eba70020a4fa2
bao.cuong-primelabo <bao.cuong@primelabo.com.vn>
2018-03-16 11:41:34 +0700

    16/03/2018 commit upload all file new project

---
+++ TestJenKinsConnectGitHub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestJenKinsConnectGitHub.Startup))]
+namespace TestJenKinsConnectGitHub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
